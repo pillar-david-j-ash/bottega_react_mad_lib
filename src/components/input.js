a2189a3697445032b8251b9ea5dd64d630efc979
David Ash <bottegadash@gmail.com>
2020-05-12 07:00:57 -0400

    modified handleInputChange to handle multiple input cases instead of just the color

--- src/components/input.js
+++ src/components/input.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 
-const Input = (title) => {
+const Input = (title, state, onChange, name) => {
     return (
         <div className="input">
-            <input />
+            <input name={name} value={state} onChange={onChange}/>
             <label>{title}</label>
         </div>
     )
