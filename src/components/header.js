beb3cc781f7ffef90eee2a04e855557a4fc37a1c
David Ash <bottegadash@gmail.com>
2020-05-13 16:52:57 -0400

    added heder h1 and p styles

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <div className="header">
             <div className="header_skew"></div>
             <div className="header_border"></div>
-            <h1>Botteg Madlibs</h1>
+            <h1>Bottega Madlibs</h1>
             <p>
                 Fill out the fields below and click the generate button to see the Mad Lib story.
             </p>
