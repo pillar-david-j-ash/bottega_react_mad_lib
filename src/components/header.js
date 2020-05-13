09aef54c9c61f49ede7a09a3c2c6a8bad16aa385
David Ash <bottegadash@gmail.com>
2020-05-13 08:57:48 -0400

    created background headers skew

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,8 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="header">
+            <div className="header_skew"></div>
+            <div className="header_border"></div>
             <h1>Botteg Madlibs</h1>
             <p>
                 Fill out the fields below and click the generate button to see the Mad Lib story.
