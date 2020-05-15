bb0240f0ebdd5c245f5de2430d1f888f77b8c5fd
David Ash <bottegadash@gmail.com>
2020-05-15 11:56:35 -0400

    styled generate madlib and clear form buttons

--- src/components/card.js
+++ src/components/card.js
@@ -81,7 +81,7 @@ class Card extends Component {
                         })
                     }
                 </div>
-                <button type="submit">{this.state.contentVisible ? 'Clear Form' : 'Generate Madlib'}</button>
+                <button className={`card_${!this.state.contentVisible ? 'generate': 'clear'}`} type="submit">{this.state.contentVisible ? 'Clear Form' : 'Generate Madlib'}</button>
                 {
                     this.state.contentVisible ? <Content data={this.state}/> : ''
                 }
